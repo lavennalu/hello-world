7aa12e687557c8b299b4aa072f7f4491883b14a1
Yiling Lu <lavennalu@gmail.com>
2018-09-23 23:26:16 -0400

    Update q3_image_sorter.bash

--- q3_image_sorter.bash
+++ q3_image_sorter.bash
@@ -2,13 +2,13 @@
 #260760075 Yiling Lu Q3
 
 #input the path for  the directory (Q3) holding all the images
-imagepath=$1
+read -p "Enter path of Q3: " $1
 
-#store the images under the path by sorting them according to  modification time
+#store the images under the path by sorting them according to modification time
 cd $1
 storeimage=$(ls -Rtr MontrealTest/*/*.jpg)
 
 #convert and append the pictures
 convert -append $storeimage Q3_MontrealTest.jpg
 
-#not for code: scp ylu123@mimi.cs.mcgill.ca:~/COMP206_A1/Q3/Q3_MontrealTest.jpg .
+#not for code, just for testing on Mac terminal: scp ylu123@mimi.cs.mcgill.ca:~/COMP206_A1/Q3/Q3_MontrealTest.jpg .
