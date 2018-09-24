3a0a85e3d2d50a26854b551832f65a29047d077e
Yiling Lu <lavennalu@gmail.com>
2018-09-24 00:03:50 -0400

    Update q3_image_sorter.bash

--- q3_image_sorter.bash
+++ q3_image_sorter.bash
@@ -1,14 +1,11 @@
 #!/bin/bash
 #260760075 Yiling Lu Q3
 
-imagepath="$1"
 #input the path for  the directory (Q3) holding all the images
-read -p "Enter path of Q3: " $imagepath
-echo $imagepath
 
 #store the images under the path by sorting them according to modification time
-cd $1
-storeimage=$(ls -Rtr "$imagepath"/MontrealTest/*/*.jpg)
+ls
+storeimage=$(ls -Rtr $1/MontrealTest/*/*.jpg)
 
 #convert and append the pictures
 convert -append $storeimage Q3_MontrealTest.jpg
